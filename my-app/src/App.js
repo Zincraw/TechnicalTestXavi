5b01ebeec119c79e2ff81dce76755cd0107e4fcf
Zincraw <xaviergs99@gmail.com>
2023-01-04 09:57:30 +0100

    main with partOne

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PeopleContainer from './components/PeopleContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <PeopleContainer />
+    </div>
+  );
+}
+
+export default App;
