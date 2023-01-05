ed80d425a32a0392150b5304d3facfdc809c5f50
Zincraw <xaviergs99@gmail.com>
2023-01-05 09:57:56 +0100

    Done without users, refactored :D

--- my-app/src/components/PeopleContainer.js
+++ my-app/src/components/PeopleContainer.js
@@ -2,31 +2,22 @@ import React, { useState } from 'react'
 import PeopleInformation from './PeopleInformation'
 import { people } from '../data/DataSamplePeople'
 
-let counter = 0
-
 const PeopleContainer = () => {  
-  const [users, setUsers] = useState([])
+  const [counter, setCounter] = useState(0)
     const handleAddUser = () => {
-      if(counter === people.length){
-        setUsers([])
-        counter = 0
-      } else {
-      const newUser = {
-        name: people[counter].name.first + ' ' + people[counter].name.last ,
-        picture: people[counter].picture.large,
-        email: people[counter].email
-      }
-      setUsers([...users, newUser])
-      counter++
-    }}
+      if(counter === people.length)
+        setCounter(0)
+       else 
+        setCounter(counter + 1)
+    }
     return (
         <div>
           <div>
             <button onClick={handleAddUser}>Click here to add a new person</button>
           </div>
-          {users.map((person, index) =>{
+          {people.filter((_, index) => index < counter).map((person, index) =>{
                 return (
-                    <PeopleInformation key={index} name={person.name} picture={person.picture} email={person.email}/>
+                    <PeopleInformation key={index} name={person.name.first + ' ' + person.name.last} picture={person.picture.large} email={person.email}/>
                 )
             })}
         </div>
